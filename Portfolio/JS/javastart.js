4ca5166a4d602e346aa7a4df918355d3a6e7f5e5
Michèle Matzeck-Kunstmann <79101578+Michoulearnstocode@users.noreply.github.com>
2022-11-01 16:00:41 +0100

    Delete Portfolio directory

--- Portfolio/JS/javastart.js
+++
@@ -1,5 +0,0 @@
-var oldElement  = document.getElementById("container1");
-var newElement = document.getElementById("container2");
-  function replaceNode(){
-    oldElement.parentNode.replaceChild(container2, container1);
-     setTimeout(replaceNode, 100);};
